42349ad10e3e7f3448199f27eeec7dde2b5eb217
pvbharath1977 <30667097+pvbharath1977@users.noreply.github.com>
2017-08-17 00:28:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/article-one',function(re, res) {
-    res.send('<html> <H><B>Article <a href="http://www.google.com">One</a> requested </B> </H></html>');
+    res.send('<html> <H1><B>Article <a href="http://www.google.com">One</a> requested </B> </H1></html>');
 });
 
 app.get('/article-two',function(re, res) {
