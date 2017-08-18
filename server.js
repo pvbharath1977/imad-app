684ba96352ccfde192055b28d89ef4297aef6b7f
pvbharath1977 <30667097+pvbharath1977@users.noreply.github.com>
2017-08-18 09:23:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -93,7 +93,7 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/:articleName',function(re, res) {
+app.get('/:articleName',function(req, res) {
     var articleName = req.params.articleName;
     res.send(createTemplate(Articles[articleName]));
 });
