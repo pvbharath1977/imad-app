e21a51c4a39aa5e01a1101e817b63dc50ffda1ba
pvbharath1977 <30667097+pvbharath1977@users.noreply.github.com>
2017-08-23 21:18:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,8 @@
 console.log('Loaded!');
 var txtElement = document.getElementById("main_text");
 txtElement.innerHTML = "This is nice";
+
+var imgElement = document.getElementById("madi");
+imgElement.onclick = function () {
+    img.style.marginLeft = "100 px";    
+};
