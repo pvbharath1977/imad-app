3f81a664d8608085e4fd088fc50e8d7c73ae46ec
pvbharath1977 <30667097+pvbharath1977@users.noreply.github.com>
2017-08-23 21:37:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,15 @@ var txtElement = document.getElementById("main_text");
 txtElement.innerHTML = "This is nice";
 
 var imgElement = document.getElementById("madi");
+
+var marginleft = 0;
+function MoveRight() {
+  marginleft = marginleft +10;
+  imgElement.style.marginLeft = marginleft;
+}
+
+
 imgElement.onclick = function () {
-    imgElement.style.marginLeft = "200px";    
+//    imgElement.style.marginLeft = "200px";    
+    var nInterval = setInterval(MoveRight,100);
 };
