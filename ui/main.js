3414eb4f115eae617bb73615ff0bb2df95d5fb37
pvbharath1977 <30667097+pvbharath1977@users.noreply.github.com>
2017-09-10 19:46:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,7 @@ console.log('Loaded!');
 //var txtElement = document.getElementById("main_text");
 //txtElement.innerHTML = "This is nice";
 
+/*
 var imgElement = document.getElementById("madi");
 
 var marginleft = 0;
@@ -9,14 +10,30 @@ function MoveRight() {
   marginleft = marginleft +2;
   imgElement.style.marginLeft = marginleft + "px";
 }
+*/
+
 
 var button = document.getElementById("counter");
 var counter = 0;
 button.onclick = function () {
-    counter = counter + 1;
-    var span = document.getElementById("count");
-    span.innerHTML = counter.toString();
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        if (request.readystate === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                var counter = request.responseText;
+                var span = document.getElementById("count");
+                span.innerHTML = counter.toString();
+            }
+        }
+    };
+
+    request.open('GET','http://pvbharath.imad')
 };
+
+
+
+
+
 
 imgElement.onclick = function () {
 //    imgElement.style.marginLeft = "200px";    
