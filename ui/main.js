0650d051f643729bec734db60ab4136d99e711d6
pvbharath1977 <30667097+pvbharath1977@users.noreply.github.com>
2017-09-10 21:12:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -36,9 +36,9 @@ var strname = nameInput.value;
 var submit = document.getElementByID('submit_btn');
 submit.onclick = function () {
     var names = ['name1','name2','name3',strname];
-    list = '';
+    var list = '';
     for (i=0;i<names.length;i++) {
-        list = list + '<li>' + names[i] + '</li>';
+        list += '<li>' + names[i] + '</li>';
     }
     var ul = document.getElementById('namelist');
     ul.innerHTML = list;
