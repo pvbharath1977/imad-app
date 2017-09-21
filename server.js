ee56fd5de3f79ceb7bfd8515a8462993b62a081e
pvbharath1977 <30667097+pvbharath1977@users.noreply.github.com>
2017-09-21 15:47:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
+var Pool = require('pg').Pool;
+
+var config = {
+    user: 'pvbharath',
+    database: 'pvbharath',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+}
+
 var app = express();
 app.use(morgan('combined'));
 
@@ -92,6 +102,18 @@ var counter = 0;
 app.get('/counter',  function(req, res){
     counter = counter + 1;
     res.send(counter.toString());
+});
+
+var pool = new Pool(config);
+
+app.get('test-db', function (req,res){
+    pool.query('select * from dbtest', function(err, result){
+       if (err) {
+           res.status(500).send(err.toString());
+       } else {
+           res.send(JSON.stringify(result));
+       }
+    });
 });
 
 app.get('/ui/style.css', function (req, res) {
