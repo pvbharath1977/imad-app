e1abd13806890f509e54dcb4108dbd19de596fed
pvbharath1977 <30667097+pvbharath1977@users.noreply.github.com>
2017-09-21 17:25:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -137,6 +137,24 @@ app.get('/:articleName',function(req, res) {
     res.send(createTemplate(Articles[articleName]));
 });
 
+app.get('/articles/:articleName',function(req, res) {
+    // var articleName = req.params.articleName;
+    pool.query("Select * from article where title = '"+ req.params.articleName + "'", function(err, result){
+       if (err) {
+           res.status(500).send(err.toString());
+       } else {
+           if (result.rows.length === 0) {
+               res.status(404).send("Article not found");
+           }
+           else {
+               var articleData = result.rows[0];
+               res.send(createTemplate(articleData));
+           }
+       }
+    });
+    
+});
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
