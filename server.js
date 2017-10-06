0f64c9ef076a6af6d1ea804cc13a22fc5683fce3
pvbharath1977 <30667097+pvbharath1977@users.noreply.github.com>
2017-10-06 15:38:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ app.get('/ui/style.css', function (req, res) {
 
 function hash(input, salt) {
     var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
-    return hashed.toString('hex');
+    return [pbkdf2, 'random-string',10000, 512,'sha512','$',hashed.toString('hex')];
 }
 
 app.get('/hash/:input', function (req, res) {
